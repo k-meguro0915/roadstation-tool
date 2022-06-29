2c5da6682e957b9db6a59bb13bc1d37e37c6bfdb
LAPTOP-LF5QFOAD\work <katsuya.meguro@aion-inc.jp>
2022-06-29 16:47:01 +0900

    DB 空文字->nullの自動変換を禁止する

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,8 +19,8 @@ class Kernel extends HttpKernel
         \Fruitcake\Cors\HandleCors::class,
         \App\Http\Middleware\PreventRequestsDuringMaintenance::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
-        \App\Http\Middleware\TrimStrings::class,
-        \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
+        // \App\Http\Middleware\TrimStrings::class,
+        // \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
     ];
 
     /**
