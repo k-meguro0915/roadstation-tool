475240c95f64484a494d59bf3f5f08a1ba7dcc71
LAPTOP-LF5QFOAD\work <katsuya.meguro@aion-inc.jp>
2022-07-07 16:46:16 +0900

    CSVインポート　認証機能

--- app/Http/Controllers/CsvController.php
+++ app/Http/Controllers/CsvController.php
@@ -12,6 +12,11 @@ use App\Services\ImportCsvService;
 use App\Services\DataVersionService;
 class CsvController extends Controller
 {
+  public function __construct()
+  {
+      $this->middleware('auth');
+  }
+
   public function index(){
     return view('importCsv');
   }
