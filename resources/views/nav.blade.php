766e7e8e528cde8ad76f4137fe4a8d242c07172f
LAPTOP-LF5QFOAD\work <katsuya.meguro@aion-inc.jp>
2022-07-08 12:46:42 +0900

    ハンバーガーメニュー作成

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,11 +1,11 @@
 @section('navigation')
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">道の駅管理ツール</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#hm-menu" aria-controls="hm-menu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+  <div class="collapse navbar-collapse" id="hm-menu">
     <ul class="navbar-nav mr-auto">
       <li id="route" class="nav-item">
         <a class="nav-link" href="/">道の駅一覧 <span class="sr-only">(current)</span></a>
