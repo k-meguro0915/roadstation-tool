9205659f84d49f9cc609ff0d02d9bfe83bf2379e
LAPTOP-LF5QFOAD\work <katsuya.meguro@aion-inc.jp>
2022-10-11 12:16:25 +0900

    api修正 get_rv_park

--- app/Services/RvParkService.php
+++ app/Services/RvParkService.php
@@ -8,7 +8,38 @@ use Illuminate\Http\Request;
 class RvParkService
 {
   public function get(){
-    return RvPark::orderBy('SID','asc')->get();
+    $ret = [];
+    $rv = RvPark::orderBy('SID','asc')->get();
+    foreach($rv as $key => $item){
+      $arr = $this->initApiArray(); //道の駅単位の初期化
+      $value = $item->getAttributes();
+      $arr['SID']                   = $value['SID'];
+      $arr['legacy_id']             = $value['legacy_id'];
+      $arr['name']                  = $value['name'];
+      $arr['introductory_sentence'] = $value['introductory_sentence'];
+      $arr['address']               = $value['address'];
+      $arr['tel']                   = $value['tel'];
+      $arr['remarks']               = $value['remarks'];
+      $arr['url']                   = $value['url'];
+      $arr['latitude']              = $value['latitude'];
+      $arr['longitude']             = $value['longitude'];
+    }
+    $ret = $arr;
     return  $ret;
+  }
+  private function init(){
+    $ret = [
+      'SID' => '',
+      'legacy_id' => '',
+      'name' => '',
+      'introductory_sentence' => '',
+      'address' => '',
+      'tel' => '',
+      'remarks' => '',
+      'url' => '',
+      'latitude' => '',
+      'longitude' => '',
+    ];
+    return $ret;
   }
 }
