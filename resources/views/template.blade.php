f6eeb4b0bbd22373dac161a942859794dbaeb1cd
LAPTOP-LF5QFOAD\work <katsuya.meguro@aion-inc.jp>
2023-03-17 14:36:45 +0900

    jqueryインポート　セキュア設定

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -21,7 +21,7 @@
 		</div>
 		<footer>
 			<script src="{{ mix('js/app.js') }}"></script>
-			<script src="{{ asset('js/jquery-3.6.0.min.js') }}"></script>
+			<script src="{{ secure_asset('js/jquery-3.6.0.min.js') }}"></script>
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
